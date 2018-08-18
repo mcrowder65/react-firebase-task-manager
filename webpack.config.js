d6ed4cf80b30c1342261df712af6af03e4669d1e
Matt Crowder <mcrowder65@gmail.com>
2018-08-17 22:04:28 -0400

    firebase hosting, got a pwa!

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const webpack = require("webpack");
 const CompressionPlugin = require("compression-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
+const ManifestPlugin = require("webpack-manifest-plugin");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const isProd = process.env.NODE_ENV === "production";
 const sourcePath = path.join(__dirname, "./src/client");
@@ -153,8 +156,28 @@ webpackConfig.plugins = [
   new webpack.EnvironmentPlugin(["NODE_ENV"]),
 
   new webpack.DefinePlugin({
-    "process.env.NODE_ENV": '"production"'
-  })
+    "process.env.NODE_ENV": "production"
+  }),
+  new ManifestPlugin({ fileName: "asset-manifest.json" }),
+  new SWPrecacheWebpackPlugin({
+    // By default, a cache-busting query parameter is appended to requests
+    // used to populate the caches, to ensure the responses are fresh.
+    // If a URL is already hashed by Webpack, then there is no concern
+    // about it being stale, and the cache-busting can be skipped.
+    dontCacheBustUrlsMatching: /\.\w{8}\./,
+    filename: "service-worker.js",
+    logger(message) {
+      if (message.indexOf("Total precache size is") === 0) {
+        // This message occurs for every build and is a bit too noisy.
+        return;
+      }
+      console.log(message);
+    },
+    minify: true, // minify and uglify the script
+    navigateFallback: "/index.html",
+    staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+  }),
+  new CopyWebpackPlugin([{ from: "src/client/pwa" }])
 ];
 if (process.env.ANALYZE_BUNDLE) {
   webpackConfig.plugins.push(new BundleAnalyzerPlugin());
